ef406948620429113b20464b8107a33072360cc5
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-16 05:28:01 -0500

    Prompt user for name and output it

---
+++ character_input.py
@@ -0,0 +1,11 @@
+###############################################################################
+# Name: Jason D. F. D'Oyley
+# Created: November 16, 2022
+# Updated: November 16, 2022
+# Practice: 1 - Character Input
+###############################################################################
+
+# Asks the user to enter their name
+name = input("Enter your name: ")
+
+print(f"Your name is {name}.")
