e9c903048334623c11f46587d204b87b7688adfe
Jason D'Oyley <jdfdoyley@gmail.com>
2022-11-16 05:51:17 -0500

    Add code to calculate year user is 100

--- character_input.py
+++ character_input.py
@@ -4,8 +4,23 @@
 # Updated: November 16, 2022
 # Practice: 1 - Character Input
 ###############################################################################
+import datetime
 
-# Asks the user to enter their name
+# Ask user to enter their name
 name = input("Enter your name: ")
 
-print(f"Your name is {name}.")
+# Ask user to enter their age
+age = int(input("Enter your age: "))
+
+# Calculate in what year the user will be 100 years old
+current_year = datetime.date.today().year       # get current year
+year = current_year - age + 100
+
+# Ask user how man copies to print
+copies = int(input("How many copies to print? "))
+output_str = (
+    f"{name.capitalize()}, you will be 100 years old in the year {year}.\n"
+)
+
+print(copies * output_str, end="")
+
